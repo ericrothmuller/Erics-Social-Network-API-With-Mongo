47c00bc17deae3c3ec4de6fd56308e249c6dca26
ericrothmuller <eric@skymarketingco.com>
2022-08-16 00:10:04 -0700

    Added index.js and finished connection between mongoose, models, and server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const db = require('./config/connection');
+const mongoose = require('mongoose');
 
-const { Thought, User } = require('./models');
+const { User, Thought } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-
-
-
 
 
 

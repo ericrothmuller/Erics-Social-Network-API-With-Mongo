9b75e90e19eba598460d13188d90e2652260857c
ericrothmuller <eric@skymarketingco.com>
2022-08-16 23:39:25 -0700

    Added email validator (from Module 17) into User module, added virtuals and getters into User and Thoughts models, and fixed ref syntax

--- models/Thought.js
+++ models/Thought.js
@@ -4,8 +4,14 @@ const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, min: 1, max: 280 },
     createdAt: { type: Date, default: Date.now, needTo: "Use a getter method to format the timestamp on query"},
     username: { type: String, required: true},
-    reactions: [reactionSchema],
-  });
+    reactions: [reactionSchema],},
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -3,10 +3,16 @@ const Thought = require('./Thought');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, trimmed: true },
-  email: { type: String, unique: true, required: true, match: "validator goes here"},
-  thoughts: { type: mongoose.Schema.Types.ObjectId, ref: Thought},
-  friends: [{ type: Schema.Types.ObjectId, ref: User}],
-});
+  email: { type: String, unique: true, required: true, match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "You must enter a valid email address"]},
+  thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thought"}],
+  friends: [{ type: Schema.Types.ObjectId, ref: "User"}],},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+}
+);
 
 const User = mongoose.model('User', userSchema);
   

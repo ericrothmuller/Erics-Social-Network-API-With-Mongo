1683b1a32345abd922fe5eff6a7ac064878029c0
ericrothmuller <eric@skymarketingco.com>
2022-08-16 22:45:03 -0700

    Added reaction.js initial page and finalized User.js thoughts and friends (still need to build the validator)

--- models/User.js
+++ models/User.js
@@ -1,30 +1,13 @@
 const mongoose = require('mongoose');
+const Thought = require('./Thought');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, trimmed: true },
-  email: { type: String, unique: true, required: true, validator: true},
-  thoughts: { needTo: "array of _id values referencing the Thought model"},
-  friends: { needTo: "array of _id values referencing the User model (self-reference) - Schema Settings - Create a virtual called friendCount that retrieves the length of the user's friends array field on query."},
+  email: { type: String, unique: true, required: true, match: "validator goes here"},
+  thoughts: { type: mongoose.Schema.Types.ObjectId, ref: Thought},
+  friends: [{ type: Schema.Types.ObjectId, ref: User}],
 });
 
 const User = mongoose.model('User', userSchema);
-
-const handleError = (err) => console.error(err);
-
-User.find({}).exec((err, collection) => {
-    if (collection.length === 0) {
-        User.insertMany(
-        [
-          { name: 'User1' },
-          { name: 'User2' },
-        ],
-        (insertErr) => {
-          if (insertErr) {
-            handleError(insertErr);
-          }
-        }
-      );
-    }
-  });
   
-  module.exports = User;
+module.exports = User;

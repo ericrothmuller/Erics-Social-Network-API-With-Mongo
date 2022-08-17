d205964d960c7c92f849cb25d1b2cf2d224fc862
ericrothmuller <eric@skymarketingco.com>
2022-08-16 22:36:01 -0700

    Added Thought.js min and max values and a reactionSchema array that I'll need to create a model for

--- models/Thought.js
+++ models/Thought.js
@@ -1,30 +1,13 @@
 const mongoose = require('mongoose');
 
 const thoughtSchema = new mongoose.Schema({
-    thoughtText: { type: String, required: true, characterLength: 1-280 },
+    thoughtText: { type: String, required: true, min: 1, max: 280 },
     createdAt: { type: Date, default: Date.now, needTo: "Use a getter method to format the timestamp on query"},
     username: { type: String, required: true},
-    reactions: { needTo: "Array of nested documents created with the reactionSchema - Schema Settings - Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query."},
+    reactions: [reactionSchema],
   });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
-const handleError = (err) => console.error(err);
 
-Thought.find({}).exec((err, collection) => {
-    if (collection.length === 0) {
-        Thought.insertMany(
-        [
-          { name: 'Thought1' },
-          { name: 'Thought2' },
-        ],
-        (insertErr) => {
-          if (insertErr) {
-            handleError(insertErr);
-          }
-        }
-      );
-    }
-  });
-  
   module.exports = Thought;

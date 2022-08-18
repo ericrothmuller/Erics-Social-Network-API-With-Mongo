8adae5db57250a9622fdd265497afbd33d8debf3
ericrothmuller <eric@skymarketingco.com>
2022-08-17 23:29:32 -0700

    Started adding routes with the GET all users and GET a single user by it's _id and populated thought and friend data. Need to figure out the thought and friend data still

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
+const moment = require("moment");
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {type: mongoose.Schema.Types.ObjectId, default: new mongoose.Schema.Types.ObjectId},
+    reactionBody: {type: String, required: true, max: 280},
+    username: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now(), get: moment().format("MMM Do YY")},},
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
 
 module.exports = reactionSchema;
